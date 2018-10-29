587d58e3876df45038b44818a7c527616bc2e11e
Quebra <38567544+Quebramedos@users.noreply.github.com>
2018-10-29 01:18:29 -0300

    testes

--- arquivo csv/index.js
+++ arquivo csv/index.js
@@ -1,12 +1,24 @@
-var csv = require("fast-csv");
+var fileStream = fs.createReadStream("csvdiscreta.csv"),
+    parser = fastCsv();
 
-csv
-    .fromPath("csvdiscreta.csv")
-    .on("data", function (data) {
-        console.log(data);
+fileStream
+    .on("readable", function () {
+        var data;
+        while ((data = fileStream.read()) !== null) {
+            parser.write(data);
+        }
+    })
+    .on("end", function () {
+        parser.end();
+    });
+
+parser
+    .on("readable", function () {
+        var data;
+        while ((data = parser.read()) !== null) {
+            console.log(data);
+        }
     })
     .on("end", function () {
         console.log("done");
     });
-
-var origin = "casa";
